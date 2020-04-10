38303f51a555909eef62dc9f52bc941c358c1ac7
jrammmy <justinramiscal@gmail.com>
2020-04-09 19:25:07 -0700

    created a restaurant card component

---
+++ components/restaurant-card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const RestaurantCard = () => {
+  return <View style={styles.imageContainer} />;
+};
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    height: 150,
+    width: 200,
+    borderWidth: 2,
+
+  },
+});
+
+export default RestaurantCard;
