13d560561eca1d9858d0dd7a2f8b49846fbc0977
jrammmy <justinramiscal@gmail.com>
2020-04-09 19:07:16 -0700

    turned the search screen into its own screen rather than a function

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import SearchScreen from './screens/SearchScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +9,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
