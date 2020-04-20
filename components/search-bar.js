f6cd4eec4213df6f7e877c5f8363934f5a0231fa
jrammmy <justinramiscal@gmail.com>
2020-04-19 18:58:58 -0700

    added a hook to manage the search bars state

--- components/search-bar.js
+++ components/search-bar.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-const SearchBar = () => {
-  return <View style={styles.searchBarContainer} />;
+const SearchBar = ({term, onTermChange}) => {
+  return (
+    <View style={styles.searchBarContainer}>
+      <Icon name="search" style={styles.icon} color="#3e3e3e" />
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Search..."
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+      />
+    </View>);
 };
 
 const styles = StyleSheet.create({
   searchBarContainer: {
     height: 50,
     borderRadius: 10,
-    backgroundColor: 'lightgrey',
-    marginHorizontal: 20,
+    backgroundColor: '#E0E0E0',
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  searchBar: {
+    flex: 1,
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
   },
 });
 
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, ScrollView, Text} from 'react-native';
+import RestaurantCard from '../components/restaurant-card';
+import SearchBar from '../components/search-bar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Search Screen</Text>
+      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}/>
+      <Text>{term}</Text>
     </View>
   );
 };
@@ -12,7 +16,11 @@ const SearchScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderWidth: 2,
+  },
+  horizontalContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
 
