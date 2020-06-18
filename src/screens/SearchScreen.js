056e991c4450cbef8df1a6f46b26c80182a3251e
jrammmy <justinramiscal@gmail.com>
2020-06-18 10:01:30 -0700

    added some basic error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, ScrollView, Text} from 'react-native';
 import RestaurantCard from '../components/restaurant-card';
 import SearchBar from '../components/search-bar';
@@ -7,25 +7,38 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'santa cruz',
-      },
-    });
-    setResults(response.data.businesses);
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'santa cruz',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err);
+      setErrorMessage('Something went wrong.');
+    }
   };
+
+  // ensures that code is ran only once when the compent renders
+  useEffect(() => {
+    // idea: based on time of day, update default to be breakfast, lunch, dinner, or late night
+    searchApi('pizza');
+  }, []);
 
   return (
     <View style={styles.container}>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results.</Text>
     </View>
   );
